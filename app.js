cc6f156e58fddfb705fc98fb312d152ca0e18259
joy111joy <mitchel_joy@hotmail.com>
2024-07-15 10:44:40 -0230

    Added more views and fixed many bugs

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
 const express = require('express');
 const methodOverride = require('method-override');
+const { getAllMovies, createMovie, deleteMovie } = require('./dal/data');
 const app = express();
 const port = 3000;
+
 
 const apiRouter = require('./routes/api');
 const uiRouter = require('./routes/ui');
 
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 app.use('/api', apiRouter);
 app.use('/', uiRouter);
+
+app.use(express.json());
+app.use(methodOverride('_method'));
+
+
+
+
+app.set('view engine', 'ejs');
+
+app.get('/', async (req, res) => {
+    const movies = await getAllMovies();
+    res.render('index', { movies });
+});
+
+app.get('/new', (req, res) => {
+    res.render('new');
+  });
+
+
+app.post('/movies', async (req, res) => {
+    const { title, description, genre, rating } = req.body;
+    await createMovie(title, description, genre, rating);
+    res.redirect('/');
+});
+
+app.delete('/movies/:id', async (req, res) => {
+    const { id } = req.params;
+    await deleteMovie(id);
+    res.redirect('/');
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`);
--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../dal/data');
 
 let data = [
     { id: 1, title: 'Avengers', Genre: 'Action', Rating: '8', Director: 'Joss Whedon' }
 ];
-
-router.get('/movies', (req, res) => {
-    res.json(data);
+//get all movies
+router.get('/movies', async (req, res) => {
+    try {
+        const movies = await db.getAllMovies();
+        res.json(movies);
+    } catch (error) {
+        console.error('Error loading movies!', error);
+        res.status(500).json({ error: error.message });
+    }
 });
 
 router.post('/movies', (req, res) => {
@@ -14,3 +21,5 @@ router.post('/movies', (req, res) => {
     data.push(newMovie);
     res.status(201).json(newMovie);
 });
+
+module.exports = router;
