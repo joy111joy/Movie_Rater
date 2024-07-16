e1ddb02cb47d474967cd0c181e44f4f82dbe4992
joy111joy <mitchel_joy@hotmail.com>
2024-07-16 02:37:37 -0230

    Most of styling finished for index page.

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const methodOverride = require('method-override');
-const { getAllMovies, createMovie, deleteMovie } = require('./dal/data');
+const path = require('path');
+
 const app = express();
 const port = 3000;
+
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 const apiRouter = require('./routes/api');
 const uiRouter = require('./routes/ui');
-app.use(methodOverride('_method'));
-app.use(express.urlencoded({ extended: true }));
+
+
+
+
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
@@ -23,29 +32,9 @@ app.use(express.json());
 
 
 
-app.set('view engine', 'ejs');
-
-app.get('/', async (req, res) => {
-    const movies = await getAllMovies();
-    res.render('index', { movies });
-});
-
-app.get('/new', (req, res) => {
-    res.render('new');
-  });
 
 
-app.post('/movies', async (req, res) => {
-    const { title, description, genre, rating } = req.body;
-    await createMovie(title, description, genre, rating);
-    res.redirect('/');
-});
 
-app.delete('/movies/:id', async (req, res) => {
-    const { id } = req.params;
-    await deleteMovie(id);
-    res.redirect('/');
-});
 
 
 app.listen(port, () => {
